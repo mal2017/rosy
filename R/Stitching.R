ea1dbc72d6c772e48ba986b4c4a7267a4c9e8c7b
mattlawlor <matt.a.lawlor@gmail.com>
2019-01-21 10:38:25 -0500

    work on stitch stats func

--- R/Stitching.R
+++ R/Stitching.R
@@ -8,15 +8,28 @@
 #' @param gr A GRanges object.
 #' @param sd Stitching distance.
 stitch_internal <- function(gr, sd = 0) {
-  gr <- GenomicRanges::trim(gr)
-  gr_expanded <- extend_merge_internal(gr = gr, width = sd)
-  GenomicRanges::strand(gr_expanded) <- "*"
+  # don't want to think abt strands
   GenomicRanges::strand(gr) <- "*"
 
+  # not beyound actual chromosome bounds
+  gr <- GenomicRanges::reduce(gr)
+  gr <- GenomicRanges::trim(gr)
+
+  # expand, remove overlaps, trim again
+  gr_expanded <- extend_merge_internal(gr = gr, width = sd)
+  gr_expanded <- GenomicRanges::reduce(gr_expanded)
+  gr_expanded <- GenomicRanges::trim(gr_expanded)
+
+  # get hits
   hits <- GenomicRanges::findOverlaps(gr, gr_expanded)
 
-  gr$stitched_region <- S4Vectors::subjectHits(hits)
+  # init new col with empty
+  mcols(gr)$stitched_region <- NA
 
+  # to those cols with any overlaps, add subject hits
+  mcols(gr)[queryHits(hits),"stitched_region"] <- S4Vectors::subjectHits(hits)
+
+  # split into grl by the subject hits
   GenomicRanges::split(gr, as.factor(gr$stitched_region))
 }
 
@@ -40,12 +53,34 @@ extend_merge_internal <- function(gr, width = 0) {
   GenomicRanges::trim(gr2)
 }
 
-get_total_constituent_width_internal <- function(gr) {
+get_total_constituent_width_internal <- function(grl) {
   sum(width(grl))
 }
 
+#' Get the total with of a GRL element.
+get_grl_entry_width_internal <- function(x) {
+  max(c(start(x), end(x))) - min(c(start(x), end(x)))
+}
+
+#' Get all GRL entry widths.
+get_stitch_region_widths_internal <- function(grl) {
+  vapply(grl, get_grl_entry_width_internal,FUN.VALUE = c(1))
+}
+
+get_total_stitch_region_width_internal <- function(grl) {
+
+}
+
+named_container <- function(vals,nms) {
+  stopifnot(length(vals) == length(nms))
+  names(vals) <- nms
+  vals
+}
+
+#' Get the stitch stats for a range of distances so rosy can
+#' internally choose an optimal distance.
 get_stitch_stats_internal <- function(gr) {
-  tbl <- tibble::tibble(STEP = seq(0, 1000, by = 500))
+  tbl <- tibble::tibble(STEP = seq(0, 2000, by = 500))
                  #GRL = NULL,
                  #NUM_REGIONS = NULL,
                  #TOTAL_CONSTIT_WIDTH  = get_total_constituent_width_internal(gr),
@@ -58,8 +93,20 @@ get_stitch_stats_internal <- function(gr) {
                  #MEAN_STITCH_FRACTION =  NULL,
                  #MED_STITCH_FRACTION = NULL)
 
+
+  tot_constit_width <- get_total_constituent_width_internal(gr)
+
+
+  # for each possible stitching configuration:
   tbl <- dplyr::mutate(tbl, GRL = purrr::map(STEP, .f = function(x) stitch_internal(gr, sd = x)))
+
+  # a list of vectors of widths
+  stitch_region_widths <- named_container(tbl$GRL, tbl$STEP)
+
   tbl <- dplyr::mutate(tbl, NUM_REGIONS = purrr::map_int(GRL,.f=length))
-  tbl <- dplyr::mutate(tbl, TOTAL_CONSTIT_WIDTH = get_total_constituent_width_internal(gr))
+  tbl <- dplyr::mutate(tbl, TOTAL_CONSTIT_WIDTH = tot_constit_width)
+  #tbl <- dplyr::mutate(tbl,TOTAL_STITCH_REGION_WIDTH = purrr::map(STEP, .f = function(x) sum(stitch_region_widths[[as.character(x)]])))
+
+
   tbl
 }
